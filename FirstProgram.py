6df0044f2c6f867b78f1328a8d877e1011929f6c
Alok Paswan <alokpaswan.dev@gmail.com>
2026-03-29 12:05:17 +0530

    Day 2 - Variables and arithmetic operators

---
+++ FirstProgram.py
@@ -0,0 +1,37 @@
+name = "Alok Paswan"
+current_age = 18
+target_age = 22
+years_to_meta = target_age - current_age
+current_salary = 0
+target_salary = 6000000
+salary_jump = target_salary - current_salary
+
+print("Name:" , name)
+print("Year until meta:" , years_to_meta)
+print("Salary jumo:" , salary_jump)
+print("Per year growth needed: " , salary_jump // years_to_meta)
+
+name = "Alok Paswan"
+current_age = 18 
+target_age = 22
+years_to_meta = target_age - current_age 
+current_salary = 0
+target_salary = 6000000
+salary_jump = target_salary - current_salary
+
+print("Name:" , name)
+print("Years until mrta:" , years_to_meta)
+print("Salary jump:" , salary_jump)
+print("Per year growth:" , salary_jump // years_to_meta)
+
+print(type(name))
+print(type(current_age))
+print(type(years_to_meta))
+print(type(salary_jump))
+
+a = 4000
+b = 6000
+sum = a + b
+diff = a - b
+print(sum)
+print(diff)
