d7210cca14fc12a51ddabd447f80eedc216d0db2
Alok Paswan <alokpaswan.dev@gmail.com>
2026-03-30 16:54:15 +0530

    Day -3 operators and logical operators

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,37 +1,57 @@
-name = "Alok Paswan"
-current_age = 18
-target_age = 22
-years_to_meta = target_age - current_age
-current_salary = 0
-target_salary = 6000000
-salary_jump = target_salary - current_salary
+#print("hello world")
+#print("hello world")
+# hello i am a comment 
+"""
+multi line 
+comment
+"""
 
-print("Name:" , name)
-print("Year until meta:" , years_to_meta)
-print("Salary jumo:" , salary_jump)
-print("Per year growth needed: " , salary_jump // years_to_meta)
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+"""
+hey i am an 
+ai and ml engineer
+"""
 
-name = "Alok Paswan"
-current_age = 18 
-target_age = 22
-years_to_meta = target_age - current_age 
-current_salary = 0
-target_salary = 6000000
-salary_jump = target_salary - current_salary
+a = 5
+b = 3
 
-print("Name:" , name)
-print("Years until mrta:" , years_to_meta)
-print("Salary jump:" , salary_jump)
-print("Per year growth:" , salary_jump // years_to_meta)
+# print(a + b)
+# print(a - b)
+# print(a-b)
+# print(a * b)
+# print(a / b)
+# print(a % b)#remainder
+# print(a ** b) # a^b
 
-print(type(name))
-print(type(current_age))
-print(type(years_to_meta))
-print(type(salary_jump))
+#relational operators
+a = 50
+b = 20
 
-a = 4000
-b = 6000
-sum = a + b
-diff = a - b
-print(sum)
-print(diff)
+# print(a == b) #False
+# print(a != b) #True 
+# print(a >= b)
+# print(a > b)
+# print(a <= b)
+# print(a < b)
+
+#assignment operators 
+# num = 10
+
+# num **= 5
+
+# print("num :" , num)
+# logical operators
+a = 30
+b = 20
+print(not True)
+print(not False)
+print(not (a>b))
+
+val1 = True
+val2 = False
+print("and operator ;" , val1 and val2)
+print("OR operator :" , val1 or val2)
+print("OR operator :" , (a==b) or (a>b))
